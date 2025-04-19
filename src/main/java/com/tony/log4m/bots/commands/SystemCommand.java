f5efa2638306f39edd17b120a1bebdac49dc8874
nunu44 <nunu44@qq.com>
2025-04-19 20:21:06 +0800

    refactor(bots): 重构命令处理逻辑并优化规则命令功能- 重构了 CommandHandler 类，优化了命令处理流程 - 新增 CategoryService 的 getByName 方法，用于查询分类 - 重构了 RuleCommand 类，增加了参数校验和异常处理 - 优化了规则列表显示逻辑，添加了分页限制 - 调整了帮助信息中的命令格式示例

--- src/main/java/com/tony/log4m/bots/commands/SystemCommand.java
+++ src/main/java/com/tony/log4m/bots/commands/SystemCommand.java
@@ -18,7 +18,7 @@ public class SystemCommand implements CommandStrategy {
         switch (command) {
             case HELP -> {
                 String help = """
-                        新建规则: @RULE@#{规则名称}-#{金额}-#{1:支付，0:收入}
+                        新建规则: /rule_add/#{规则名称}-#{金额}-#{1:支付，0:收入}
                         规则示例: @RULE@烧鸭肶-18-1
                         """;
                 return new SendMessage(chatId, help);
