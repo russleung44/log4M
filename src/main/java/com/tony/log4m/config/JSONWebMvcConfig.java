d8542e4a96b59192fd524f476e6150ac52d49205
nunu <nunuleung44@gmail.com>
2025-04-21 23:31:02 +0800

    feat:集成 fastjson2 并优化 Web 配置
    - 添加 JSONWebMvcConfig 类，配置 fastjson2 作为 JSON 序列化工具
    - 新增 BillController 控制器，提供 bill 列表接口
    - 在全局异常处理中添加对 NoResourceFoundException 的处理
    - 更新 pom.xml，添加 fastjson2-extension-spring6 依赖

---
+++ src/main/java/com/tony/log4m/config/JSONWebMvcConfig.java
@@ -0,0 +1,63 @@
+package com.tony.log4m.config;
+
+import com.alibaba.fastjson2.JSONReader;
+import com.alibaba.fastjson2.JSONWriter;
+import com.alibaba.fastjson2.support.config.FastJsonConfig;
+import com.alibaba.fastjson2.support.spring6.http.converter.FastJsonHttpMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * fastjson 配置
+ */
+@Configuration
+public class JSONWebMvcConfig implements WebMvcConfigurer {
+
+    @Bean
+    public FastJsonConfig fastJsonConfig() {
+        FastJsonConfig config = new FastJsonConfig();
+
+        // 配置序列化的行为
+        config.setWriterFeatures(
+                //json格式化
+                JSONWriter.Feature.PrettyFormat,
+                //输出map中value为null的数据
+                JSONWriter.Feature.WriteMapNullValue,
+                //输出boolean 为 false
+                JSONWriter.Feature.WriteNullBooleanAsFalse,
+                //输出list 为 []
+                JSONWriter.Feature.WriteNullListAsEmpty,
+                //输出number 为 0
+                JSONWriter.Feature.WriteNullNumberAsZero,
+                //输出字符串 为 ""
+                JSONWriter.Feature.WriteNullStringAsEmpty,
+                // long -> String
+                JSONWriter.Feature.WriteLongAsString,
+                //对map进行排序
+                JSONWriter.Feature.SortMapEntriesByKeys
+        );
+
+        // 配置反序列化的行为
+        config.setReaderFeatures(JSONReader.Feature.FieldBased, JSONReader.Feature.SupportArrayToBean);
+
+        return config;
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
+        converter.setFastJsonConfig(fastJsonConfig());
+        converter.setDefaultCharset(StandardCharsets.UTF_8);
+        converter.setSupportedMediaTypes(Collections.singletonList(MediaType.APPLICATION_JSON));
+
+        converters.addFirst(converter);
+    }
+
+}
